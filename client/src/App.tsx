0e738f4363ae3e01fe5dd5c7f0ca2ead31b91b36
mbakin <mehmetberkakinn@outlook.com>
2023-05-24 15:49:25 +0300

    set up prettierrc

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import * as React from 'react'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import * as React from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -19,18 +19,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Edit Tayfa
-      </p>
+      <p className="read-the-docs">Edit Tayfa</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
