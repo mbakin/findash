5d12de76c664965282a37c65db941e1af34c5cd4
mbakin <mehmetberkakinn@outlook.com>
2023-05-24 19:39:11 +0300

    feat: create Navbar views

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,29 @@
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { themeSettings } from "./theme";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "@/views/navbar/index";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        Hello MUI
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<div>Dashboard Page</div>} />
+              <Route
+                path="/predictions"
+                element={<div>Predictions Page</div>}
+              />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
